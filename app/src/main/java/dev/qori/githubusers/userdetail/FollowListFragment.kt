eebd3071bc28c6e24e943eedb8f596e3c0311f72
q <mboh.dong@gmail.com>
2021-09-29 19:33:08 +0700

    make follow list DRY

---
+++ app/src/main/java/dev/qori/githubusers/userdetail/FollowListFragment.kt
@@ -0,0 +1,38 @@
+package dev.qori.githubusers.userdetail
+
+import android.content.Context
+import androidx.core.os.bundleOf
+import dev.qori.githubusers.userlist.UserListFragment
+import dev.qori.githubusers.userlist.UserListViewModel
+
+class FollowListFragment(): UserListFragment() {
+    private lateinit var username: String
+    private lateinit var listType: ListType
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        arguments?.getString(ARG_USERNAME)?.let {
+            username = it
+        }
+        arguments?.getSerializable(ARG_LIST_CONTENT)?.let {
+            listType = it as ListType
+        }
+    }
+    override fun getUserListType(): UserListViewModel.UserListType = when(listType){
+        ListType.FOLLOWERS -> UserListViewModel.UserFollower(username)
+        ListType.FOLLOWING -> UserListViewModel.UserFollowing(username)
+    }
+
+    companion object{
+        private const val ARG_USERNAME = "ARG_USERNAME"
+        private const val ARG_LIST_CONTENT= "ARG_LIST_CONTENT"
+
+        fun newInstance(username: String, listType: ListType) = FollowListFragment().apply {
+            arguments = bundleOf(ARG_USERNAME to username, ARG_LIST_CONTENT to listType)
+        }
+    }
+
+    enum class ListType{
+        FOLLOWERS, FOLLOWING
+    }
+}
--- app/src/main/java/dev/qori/githubusers/userdetail/UserDetailPagerAdapter.kt
+++ app/src/main/java/dev/qori/githubusers/userdetail/UserDetailPagerAdapter.kt
@@ -3,15 +3,14 @@ package dev.qori.githubusers.userdetail
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import dev.qori.githubusers.userlist.UserListFragment
 
 class UserDetailPagerAdapter(activity: AppCompatActivity, private val username: String) : FragmentStateAdapter(activity){
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when(position){
-            0-> FollowersListFragment.newInstance(username)
-            else->FollowingListFragment.newInstance(username)
+            0-> FollowListFragment.newInstance(username, FollowListFragment.ListType.FOLLOWERS)
+            else->FollowListFragment.newInstance(username, FollowListFragment.ListType.FOLLOWING)
         }
     }
 }
