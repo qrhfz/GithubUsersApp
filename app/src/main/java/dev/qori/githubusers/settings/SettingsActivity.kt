bd26986a22685c91cbc5a7c5dba0d47b99ca7af0
q <mboh.dong@gmail.com>
2021-10-04 15:20:14 +0700

    activity settings

---
+++ app/src/main/java/dev/qori/githubusers/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package dev.qori.githubusers.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.qori.githubusers.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
